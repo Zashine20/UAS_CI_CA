e5670c73ff7485b67b2958adf22354ce04ba8f43
Zashine20 <159669730+Zashine20@users.noreply.github.com>
2025-06-14 14:09:54 +0700

    update tittle

--- application/controllers/Dashboard_user.php
+++ application/controllers/Dashboard_user.php
@@ -3,7 +3,7 @@ defined('BASEPATH')OR exit('No direct script acsess allowed');
 
 class Dashboard_user extends CI_Controller{
     public function index(){
-        $data['content']= '<h1> welcome to adminlte 3 in codeigniter</h1>';
+        $data['content']= '<h1> welcome to System PT Maju Jaya</h1>';
         $this->load->view('Tamplates/header');
         $this->load->view('dashboard_user', $data);
         $this->load->view('Tamplates/footer');
